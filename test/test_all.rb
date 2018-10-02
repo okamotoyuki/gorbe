4840aa7929950df9ef17848274a58c72e644350a
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2018-10-03 07:47:38 +0900

    updated test framework

---
+++ test/test_all.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'gorbe'
+
+class CoreTest < Minitest::Test
+  def setup
+    @gorbe = Gorbe::Core.new(:info)
+  end
+
+  def teardown
+    @gorbe = nil
+  end
+
+  # Define test methods
+  Dir.glob('./test/ruby_samples/**/*').each do |test_path|
+    next unless test_path.end_with? '.rb'
+
+    test_name = "test_#{test_path
+                            .gsub('./test/ruby_samples/', '')
+                            .gsub('.rb', '')
+                            .gsub('/', '_')}"
+    define_method test_name do
+      @gorbe.compile_file(test_path)
+      # TODO : Add assert function here
+    end
+  end
+end
