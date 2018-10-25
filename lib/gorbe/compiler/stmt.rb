af257fee63919c62a6cd8557c543cc676948432f
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2018-10-26 07:42:22 +0900

    refactored 'stmt.rb'

--- lib/gorbe/compiler/stmt.rb
+++ lib/gorbe/compiler/stmt.rb
@@ -22,11 +22,12 @@ module Gorbe
                 var_field: 'var_field',
                 var_ref: 'expr',
                 string_literal: 'expr',
-                if: 'branch',
-                if_mod: 'branch',
-                elsif: 'branch',
-                unless: 'branch',
-                unless_mod: 'branch',
+                case: 'case',
+                if: 'if_or_unless',
+                if_mod: 'if_or_unless',
+                elsif: 'if_or_unless',
+                unless: 'if_or_unless',
+                unless_mod: 'if_or_unless',
                 else: 'else'
             }
         )
@@ -84,49 +85,47 @@ module Gorbe
         # Do nothing
       end
 
-      def visit_branch(node, **args)
-        log_activity(__method__.to_s)
+      # def visit_case(node)
+      #   log_activity(__method__.to_s)
+      #
+      #   # e.g. [:case, [:var_ref, [:@ident, "foo", [3, 5]]],
+      #   #       [:when, [[:@int, "1", [4, 5]]], [[:@int, "2", [5, 1]]],
+      #   #       [:else, [[:@int, "3", [7, 1]]]]]]
+      #   raise CompileError.new(node, msg: 'Node size must be more than 2.') unless node.length > 2
+      #
+      #   with(target: visit(node[1])) do |temps|
+      #     node[2..node.length-1].each do |node|
+      #
+      #     end
+      #   end
+      # end
 
-        # e.g. [:if, [:var_ref, [:@kw, "true", [1, 3]]], [[:@int, "1", [2, 2]]],
-        #       [:elsif, [:var_ref, [:@kw, "false", [3, 6]]], [[:@int, "2", [4, 2]]],
-        #       [:else, [[:@int, "3", [6, 2]]]]]]
-        #
-        #      [:if_mod, [:var_ref, [:@kw, "true", [9, 5]]], [:@int, "1", [9, 0]]]
-        raise CompileError.new(node, msg: 'Node size must be 4.') unless node.length == 3 || node.length == 4
-
-        # 'if'/'unless'/'else'/'elsif' bodies
-        bodies = args[:bodies].nil? ? [] : args[:bodies]
-
-        label = -1  # Initialize label
-        # 'if'/'unless' condition
-        with(cond: visit(node[1])) do |cond_temps|
-          label = @block.gen_label
-          case node[0]
-
-          # Set method name depending on the branch type
-          when :if, :elsif, :if_mod then
-            is_not = false
-          when :unless, :unless_mod then
-            is_not = true
-          else
-            raise CompileError.new(node, msg: 'Unsupported branch node.')
-          end
-
-          with(is_true: @block.alloc_temp('bool')) do |true_temps|
-            template = <<~EOS
-              if #{true_temps[:is_true].expr}, πE = πg.IsTrue(πF, #{cond_temps[:cond].expr}); πE != nil {
-              \tcontinue
-              }
-              if #{is_not ? '!' : ''}#{true_temps[:is_true].expr} {
-              \tgoto Label#{label}
-              }
-            EOS
-            @writer.write(template)
-          end
+      private def visit_branch(node, bodies, branch_type, branch_condition, branch_body, next_branch_body=nil)
+        # Check if '!' is needed for the condition depending on the branch type
+        case branch_type
+        when :if, :elsif, :if_mod then
+          is_not = false
+        when :unless, :unless_mod then
+          is_not = true
+        else
+          raise CompileError.new(node, msg: 'Unsupported branch node.')
         end
-        bodies.push([label, node[2]])
 
-        if node[3].nil? # If there is no 'else' statement
+        label = @block.gen_label
+        with(is_true: @block.alloc_temp('bool')) do |bool_temps|
+          template = <<~EOS
+            if #{bool_temps[:is_true].expr}, πE = πg.IsTrue(πF, #{branch_condition.expr}); πE != nil {
+            \tcontinue
+            }
+            if #{is_not ? '!' : ''}#{bool_temps[:is_true].expr} {
+            \tgoto Label#{label}
+            }
+          EOS
+          @writer.write(template)
+        end
+        bodies.push([label, branch_body])
+
+        if next_branch_body.nil?  # If there is no 'else' statement
           end_label = @block.gen_label
 
           # Write labels and bodies
@@ -136,8 +135,26 @@ module Gorbe
             @writer.write("goto Label%d" % end_label)
           end
           @writer.write_label(end_label)
-        else # If there is 'elsif' or 'else' statement
-          visit(node[3], bodies: bodies)
+        else  # If there is 'elsif' or 'else' statement
+          visit(next_branch_body, bodies: bodies)
+        end
+      end
+
+      def visit_if_or_unless(node, **args)
+        log_activity(__method__.to_s)
+
+        # e.g. [:if, [:var_ref, [:@kw, "true", [1, 3]]], [[:@int, "1", [2, 2]]],
+        #       [:elsif, [:var_ref, [:@kw, "false", [3, 6]]], [[:@int, "2", [4, 2]]],
+        #       [:else, [[:@int, "3", [6, 2]]]]]]
+        #
+        #      [:if_mod, [:var_ref, [:@kw, "true", [9, 5]]], [:@int, "1", [9, 0]]]
+        raise CompileError.new(node, msg: 'Node size must be 4.') unless node.length == 3 || node.length == 4
+
+        # Branch bodies
+        bodies = args[:bodies].nil? ? [] : args[:bodies]
+
+        with(cond: visit(node[1])) do |temps|
+          visit_branch(node, bodies, node[0], temps[:cond], node[2], node[3])
         end
       end
 
